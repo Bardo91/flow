884a6cb0b5a8aee97fe2a6b81834532290bcc291
Bardo91 <19Bardo91@gmail.com>
2019-11-09 01:22:12 +0100

    Created repo with files from mico

---
+++ include/flow/OutPipe.h
@@ -0,0 +1,67 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  FLOW
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2018 Pablo Ramon Soria (a.k.a. Bardo91) pabramsor@gmail.com
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+
+#ifndef FLOW_OUTPIPE_H_
+#define FLOW_OUTPIPE_H_
+
+#include <vector>
+#include <cstdlib>
+
+#include <any>
+#include <unordered_map>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <functional>
+
+#include <mutex>
+
+
+
+namespace flow{
+    class Policy;
+    
+    class OutPipe{
+        public:
+            OutPipe(std::string _tag);
+
+            std::string tag() const;
+            
+            bool registerPolicy(Policy* _pol);
+
+            void unregisterPolicy(Policy* _pol);
+
+            void flush(std::any _data);
+
+            int registrations();
+
+        protected:
+            std::mutex policiesGuard;
+            std::string tag_;
+            std::vector<Policy*> registeredPolicies_;   // Policy registered, ID of stream and index in policy;
+            
+    };
+}
+
+
+
+#endif
